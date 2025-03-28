925c8724439be4483b4fd3deab66ffa2e8a2e4ca
JohnelB <johnelbucog@gmail.com>
2025-03-28 11:57:24 +0800

    Inventory Update

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -7,8 +7,26 @@ const Inventory = () => {
       <div style={styles.container}>
         <h1 style={styles.text}>Inventory Page</h1>
         <p style={styles.text}>
-          This is a test page to check if navigation is working properly.
+          LEBRON LEBRON LEBRON JAMES - Glorious King
         </p>
+        <div style={styles.card}>
+          <table style={styles.table}>
+            <thead>
+            <tr>
+              <th style={styles.thead}>Medicine</th>
+              <th style={styles.thead}>Category</th>
+              <th style={styles.thead}>Stocks</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td style={styles.tdata}>Biogesic</td>
+              <td style={styles.tdata}>Paracetamol</td>
+              <td style={styles.tdata}>100</td>
+            </tr>
+            </tbody> 
+          </table>
+        </div>
       </div>
     </Sidebar>
   );
@@ -22,6 +40,35 @@ const styles = {
   text: {
     color: "black",
   },
+  tbltxt: {
+    color: "black",
+  },
+  card: {
+    width: "67%",
+    margin: "auto",
+    overflowX: "auto",
+    border: "1px solid #ddd",
+    borderRadius: "8px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+  },
+  table: {
+    border: "2px solid forestgreen",
+    width: "800px",
+    height: "100px",
+    color: "black",
+  },
+  thead: {
+    padding: "12px",
+    background: "#1e3a8a",
+    color: "#fff",
+    borderBottom: "2px solid #ddd",
+  },
+  tdata: {
+    padding: "12px",
+    borderBottom: "1px solid #ccc",
+    borderRight: "1px solid #ddd",
+    color: "#000",
+  }
 };
 
 export default Inventory;
